3acf31b6f7c4383e176348334edc719e0406a35b
Alex Wang <iamalex11303@gmail.com>
2024-10-06 23:28:59 +0800

    updated copyright

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         {/* Links */}
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div className="col-md-4 mx-5 d-flex align-items-center">
-            <span className="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+            <span className="mb-3 mb-md-0 text-muted">© 2024</span>
           </div>
 
           <ul className="nav col-md-4 align-items-center justify-content-end list-unstyled d-flex">
