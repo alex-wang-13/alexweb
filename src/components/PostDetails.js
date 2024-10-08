c7a8d87c47ad96227c78e6a2af1eac7d4bc31b49
Alex Wang <iamalex11303@gmail.com>
2024-10-08 18:08:40 +0800

    rudimentary layout implemented

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 const PostDetails = () => {
+    const { id } = useParams();
+    const [post, setPost] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/posts/${id}`);
+                const data = await response.json();
+                setPost(data);
+                console.log(data)
+            } catch (error) {
+                console.error('Error fetching post', error);
+            }
+        };
+
+        fetchPost();
+    }, []);
+
     return (
-        <div className="container mt-5">PostDetails</div>
+        <div className="container mt-5">
+            <h2>{post.title}</h2>
+            <p className="text-muted">{post.content}</p>
+        </div>
     );
 };
 
