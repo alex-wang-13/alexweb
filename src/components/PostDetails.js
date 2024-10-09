a9eb156c22a5e071369afc32143a2e5afc1f9a50
Alex Wang <iamalex11303@gmail.com>
2024-10-10 00:42:54 +0800

    added post date and author to pages

--- src/components/PostDetails.js
+++ src/components/PostDetails.js
@@ -4,14 +4,17 @@ import { useParams } from 'react-router-dom';
 const PostDetails = () => {
     const { id } = useParams();
     const [post, setPost] = useState([]);
+    const [plainDate, setPlainDate] = useState([]);
+
 
     useEffect(() => {
         const fetchPost = async () => {
             try {
                 const response = await fetch(`http://localhost:5000/posts/${id}`);
                 const data = await response.json();
+                const date = new Date(data.date).toUTCString().substring(0, 16);
                 setPost(data);
-                console.log(data)
+                setPlainDate(date);
             } catch (error) {
                 console.error('Error fetching post', error);
             }
@@ -23,6 +26,8 @@ const PostDetails = () => {
     return (
         <div className="container mt-5">
             <h2>{post.title}</h2>
+            <p className="small">{post.author}</p>
+            <p className="small">{plainDate}</p>
             <p className="text-muted">{post.content}</p>
         </div>
     );
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom';
 
 const PostList = () => {
   const [postList, setPostList] = useState([]);
+  const [plainDate, setPlainDate] = useState([]);
 
   useEffect(() => {
     const fetchPostList = async () => {
       try {
         const response = await fetch('http://localhost:5000/posts');
         const data = await response.json();
+        const date = new Date(data[0].date).toUTCString().substring(0, 16);
         setPostList(data);
+        setPlainDate(date);
       } catch (error) {
         console.error('Error fetching post list:', error);
       }
@@ -28,11 +31,12 @@ const PostList = () => {
           {postList.map((post) => (
             <li key={post._id} className="list-group-item">
               <Link to={`/posts/${post._id}`} style={{ color: "inherit", textDecoration: "inherit" }}>
-                <h4>{post.title}</h4>
+                <h3>{post.title}</h3>
+                <p className="small text-muted">{plainDate}</p>
                 {post.content.length > 100 ? (
-                  <p className="small text-muted">{post.content.substring(0, 100)}...</p>
+                  <p className="fs-6 text-muted">{post.content.substring(0, 100)}...</p>
                 ) : (
-                  <p className="small text-muted">{post.content}</p>
+                  <p className="fs-6 text-muted">{post.content}</p>
                 )}
               </Link>
             </li>
