f6b924577988512046bcb6466d8e08d77bab5976
Alex Wang <iamalex11303@gmail.com>
2024-10-24 17:24:49 -0400

    updated home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="container mt-5">
-      <div className="row align-items-center">
+      <div className="row">
         {/* Developer Bio Section */}
-        <div className="col-md-8">
+        <div className="col-md-9">
           <h1>About the Developer</h1>
           <p>
             Hello! I'm Alex Wang, a passionate full-stack developer with coding experience in real-world environments.
@@ -23,7 +23,7 @@ const Home = () => {
         </div>
 
         {/* Profile Image Section */}
-        <div className="col-md-4 text-center">
+        <div className="col-md-3 text-center">
           <img
             src="images/IMG_2.jpg"
             alt="Developer Profile"
@@ -40,26 +40,18 @@ const Home = () => {
         <div className="row text-center align-items-center">
           <div className="col-md-3">
             <img src="images/js-logo.png" alt="JavaScript Logo" className="img-fluid" />
-          </div>
-          <div className="col-md-3">
-            <img src="images/react-logo.png" alt="React Logo" className="img-fluid" />
-          </div>
-          <div className="col-md-3">
-            <img src="images/node-logo.png" alt="Node.js Logo" className="img-fluid" />
-          </div>
-          <div className="col-md-3">
-            <img src="images/mongodb-logo.png" alt="MongoDB Logo" className="img-fluid" />
-          </div>
-          <div className="col-md-3">
             <p>JavaScript</p>
           </div>
           <div className="col-md-3">
+            <img src="images/react-logo.png" alt="React Logo" className="img-fluid" />
             <p>React</p>
           </div>
           <div className="col-md-3">
+            <img src="images/node-logo.png" alt="Node.js Logo" className="img-fluid" />
             <p>Node.js</p>
           </div>
           <div className="col-md-3">
+            <img src="images/mongodb-logo.png" alt="MongoDB Logo" className="img-fluid" />
             <p>MongoDB</p>
           </div>
         </div>
@@ -76,21 +68,21 @@ const Home = () => {
 
       {/* Links */}
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-          <div className="col-md-4 mx-5 d-flex align-items-center">
-            <span className="mb-3 mb-md-0 text-muted">© 2024</span>
-          </div>
+        <div className="col-md-4 mx-5 d-flex align-items-center">
+          <span className="mb-3 mb-md-0 text-muted">© 2024</span>
+        </div>
 
-          <ul className="nav col-md-4 align-items-center justify-content-end list-unstyled d-flex">
-            <li className="text-muted">
-              <div>- follow me -</div>
-              <div className="small">icons by icons8</div>
-            </li>
-            <li className="ms-3"><a className="text-muted" href="https://github.com/alex-wang-13"><img alt="Github Logo" src="images/icons8-github-48.png" /></a></li>
-            <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/alex-11303-wang"><img alt="Linkedin Logo" src="images/icons8-linkedin-48.png" /></a></li>
-            <li className="ms-3"><a className="text-muted" href="https://letterboxd.com/alexthedawg"><img alt="Letterboxd Logo" src="images/icons8-letterboxd-48.png" /></a></li>
-            <li className="ms-3"><a className="text-muted" href="https://www.goodreads.com/user/show/57369610-alex"><img alt="Goodreads Logo" src="images/icons8-goodreads-48.png" /></a></li>
-          </ul>
-        </footer>
+        <ul className="nav col-md-4 align-items-center justify-content-end list-unstyled d-flex">
+          <li className="text-muted">
+            <div>- follow me -</div>
+            <div className="small">icons by icons8</div>
+          </li>
+          <li className="ms-3"><a className="text-muted" href="https://github.com/alex-wang-13"><img alt="Github Logo" src="images/icons8-github-48.png" /></a></li>
+          <li className="ms-3"><a className="text-muted" href="https://www.linkedin.com/in/alex-11303-wang"><img alt="Linkedin Logo" src="images/icons8-linkedin-48.png" /></a></li>
+          <li className="ms-3"><a className="text-muted" href="https://letterboxd.com/alexthedawg"><img alt="Letterboxd Logo" src="images/icons8-letterboxd-48.png" /></a></li>
+          <li className="ms-3"><a className="text-muted" href="https://www.goodreads.com/user/show/57369610-alex"><img alt="Goodreads Logo" src="images/icons8-goodreads-48.png" /></a></li>
+        </ul>
+      </footer>
     </div>
   );
 };
