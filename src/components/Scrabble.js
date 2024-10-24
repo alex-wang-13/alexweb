21dcbdd5a00a5181227732b0683cc1c435b2a06c
Alex Wang <iamalex11303@gmail.com>
2024-10-24 13:05:52 -0400

    added definition and revealer as an option to the page

--- src/components/Scrabble.js
+++ src/components/Scrabble.js
@@ -5,11 +5,12 @@ const Scrabble = () => {
     const [fill, setFill] = useState([]);
     const [rack, setRack] = useState([]);
     const [userSolve, setUserSolve] = useState([]);
-    const [zippedFill, setZippedFill] = useState([]);
+    // const [zippedFill, setZippedFill] = useState([]);
 
     const [dataList, setDataList] = useState([]);
     const [indexNum, setIndexNum] = useState(-1);
     const [isLoaded, setIsLoaded] = useState(false);
+    const [isRevealed, setIsRevealed] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -48,9 +49,11 @@ const Scrabble = () => {
                 const rackIndex = rack.findIndex(letter => letter === key);
 
                 // Add key to solve
-                setUserSolve([...userSolve, key]);
+                setUserSolve(solve => [...solve, key]);
                 // Remove key from rack
                 setRack(rack.filter((_val, ind) => ind !== rackIndex));
+            } else if (key === ' ') {
+                setIsRevealed(!isRevealed);
             }
         };
 
@@ -59,47 +62,52 @@ const Scrabble = () => {
         return () => {
             document.removeEventListener('keydown', handleKeydown);
         };
-    }, [userSolve, rack, fill]);
+    });
 
     useEffect(() => {
         if (!isLoaded) return;
 
-        const getScramble = (arr) => {
-            const len = arr.length;
-            if (len < 1) return;
+        // const getScramble = (arr) => {
+        //     const len = arr.length;
+        //     if (len < 1) return;
 
-            // Get a random number of 'given' tiles between 0 and len-1
-            // Log distribution function to prefer fewer given tiles
-            // see: https://en.wikipedia.org/wiki/Logarithmic_distribution
-            const p = 3 / 4 - 1 / len;
-            const upper = 1 / p;
-            const z = Math.random() * upper;
-            const pgfz = Math.abs(Math.log(1 - p * z) / Math.log(1 - p));
-            const numGivens = (pgfz > len - 1) ? len - 1 : Math.floor(pgfz);
+        //     // Get a random number of 'given' tiles between 0 and len-1
+        //     // Log distribution function to prefer fewer given tiles
+        //     // see: https://en.wikipedia.org/wiki/Logarithmic_distribution
+        //     const p = 3 / 4 - 1 / len;
+        //     const upper = 1 / p;
+        //     const z = Math.random() * upper;
+        //     const pgfz = Math.abs(Math.log(1 - p * z) / Math.log(1 - p));
+        //     const numGivens = (pgfz > len - 1) ? len - 1 : Math.floor(pgfz);
 
-            // Get an array representing which tiles are given
-            const scrambleArray = Array(numGivens).fill('X');
-            while (scrambleArray.length < len) scrambleArray.push('_');
-            // Mix the array
-            scrambleArray.sort((_x, _y) => Math.random() - 0.5);
-            return scrambleArray;
-        };
+        //     // Get an array representing which tiles are given
+        //     const scrambleArray = Array(numGivens).fill('X');
+        //     while (scrambleArray.length < len) scrambleArray.push('_');
+        //     // Mix the array
+        //     scrambleArray.sort((_x, _y) => Math.random() - 0.5);
+        //     return scrambleArray;
+        // };
+
+        // const wordArray = dataList[indexNum].word.toUpperCase().split('');
+        // const scramble = getScramble(wordArray);
+
+        // const fill = [];
+        // const rack = [];
+        // scramble.forEach((val, index) => {
+        //     if (val === 'X') {
+        //         // Tile is given
+        //         fill.push(wordArray[index]);
+        //     } else {
+        //         // Tile is not given
+        //         fill.push('_');
+        //         rack.push(wordArray[index]);
+        //     }
+        // });
+        // rack.sort((_x, _y) => Math.random() - 0.5);
 
         const wordArray = dataList[indexNum].word.toUpperCase().split('');
-        const scramble = getScramble(wordArray);
-
-        const fill = [];
-        const rack = [];
-        scramble.forEach((val, index) => {
-            if (val === 'X') {
-                // Tile is given
-                fill.push(wordArray[index]);
-            } else {
-                // Tile is not given
-                fill.push('_');
-                rack.push(wordArray[index]);
-            }
-        });
+        const fill = Array(wordArray.length).fill('_');
+        const rack = wordArray;
         rack.sort((_x, _y) => Math.random() - 0.5);
 
         setFill(fill);
@@ -107,43 +115,24 @@ const Scrabble = () => {
         setUserSolve([]);
     }, [indexNum]);
 
-    // Update the value of zippedFill and check if it is a match
     useEffect(() => {
         if (!isLoaded) return;
 
         const arraysEqual = (arr, brr) => {
-            // Check if the lengths are the same
-            if (arr.length !== brr.length) {
-                return false;
-            }
+            if (arr.length !== brr.length) return false;
 
             // Check if each element is the same
             for (let i = 0; i < arr.length; i++) {
-                if (arr[i] !== brr[i]) {
-                    return false;
-                }
+                if (arr[i] !== brr[i]) return false;
             }
-
-            // If all checks pass, the arrays are equal
             return true;
         };
-        const zipFill = (arr, brr) => {
-            // Replace '_' in arr with values in brr
-            const fill = [];
-            for (var inda = 0, indb = 0; inda < arr.length && indb < brr.length; inda++) {
-                if (arr[inda] === '_') fill.push(brr[indb++]);
-            }
 
-            return (fill.length < arr.length) ? fill.concat(Array(arr.length - fill.length).fill('_')) : fill;
-        };
-
-        const zippedFill = zipFill(fill, userSolve);
-        if (arraysEqual(zippedFill, dataList[indexNum].word.toUpperCase().split(''))) {
-            setIndexNum(index => index + 1);
-        } else {
-            setZippedFill(zippedFill);
+        if (arraysEqual(userSolve, dataList[indexNum].word.split(''))) {
+            setIndexNum(num => num + 1);
+            setIsRevealed(false);
         }
-    }, [fill, userSolve]);
+    }, [userSolve]);
 
     return (
         <div className="container mt-5">
@@ -151,20 +140,15 @@ const Scrabble = () => {
                 {isLoaded ? (
                     <div>
                         <div className="display-2">
-                            {zippedFill.join(' ')}
+                            {(userSolve.length < fill.length) ? (
+                                userSolve.concat(Array(fill.length - userSolve.length).fill('_')).join(' ')
+                            ) : (
+                                userSolve.join(' ')
+                            )}
                         </div>
                         <br />
                         <div className="display-4">
                             {rack.join(' ')}
-                        </div>
-                        <div className="fs-5 pt-5">
-                            {'index: ' + indexNum}
-                        </div>
-                        <div className="fs-5 pt-5">
-                            {`datalist[${indexNum}]: '${dataList[indexNum].word}'`}
-                        </div>
-                        <div className="fs-5 pt-5">
-                            {`userSolve: ${userSolve}`}
                         </div>
                     </div>
                 ) : (
@@ -173,6 +157,14 @@ const Scrabble = () => {
                     </div>
                 )}
             </div>
+            {isRevealed && isLoaded ? (
+                <div className="text-left">
+                    <p>{dataList[indexNum].word}</p>
+                    <p className="text-muted">{dataList[indexNum].definition}</p>
+                </div>
+            ) : (
+                <div className="w-100"></div>
+            )}
         </div>
     );
 };
