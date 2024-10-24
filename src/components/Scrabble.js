c32e2ef820408a02bdc18d40c88ac7cd6e6e660e
Alex Wang <iamalex11303@gmail.com>
2024-10-24 14:29:14 -0400

    updated description page

--- src/components/Scrabble.js
+++ src/components/Scrabble.js
@@ -11,6 +11,7 @@ const Scrabble = () => {
     const [indexNum, setIndexNum] = useState(-1);
     const [isLoaded, setIsLoaded] = useState(false);
     const [isRevealed, setIsRevealed] = useState(false);
+    const [isDescribed, setIsDescribed] = useState(true);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,6 +37,9 @@ const Scrabble = () => {
         const handleKeydown = (e) => {
             const key = e.key.toUpperCase();
 
+            // Retract description page on any key press
+            setIsDescribed(false);
+
             if (key === 'BACKSPACE' || key === 'DELETE') {
                 if (userSolve.length > 0) {
                     const rackLetter = userSolve[userSolve.length - 1];
@@ -53,7 +57,11 @@ const Scrabble = () => {
                 // Remove key from rack
                 setRack(rack.filter((_val, ind) => ind !== rackIndex));
             } else if (key === ' ') {
-                setIsRevealed(!isRevealed);
+                // Do not allow definition toggle on description page
+                if (!isDescribed) setIsRevealed(!isRevealed);
+            } else if (key === 'TAB') {
+                e.preventDefault();
+                setIsDescribed(!isDescribed);
             }
         };
 
@@ -107,7 +115,7 @@ const Scrabble = () => {
 
         const wordArray = dataList[indexNum].word.toUpperCase().split('');
         const fill = Array(wordArray.length).fill('_');
-        const rack = wordArray;
+        const rack = wordArray.reverse();
         rack.sort((_x, _y) => Math.random() - 0.5);
 
         setFill(fill);
@@ -135,35 +143,61 @@ const Scrabble = () => {
     }, [userSolve]);
 
     return (
-        <div className="container mt-5">
-            <div className="text-center my-5 py-5">
-                {isLoaded ? (
-                    <div>
-                        <div className="display-2">
-                            {(userSolve.length < fill.length) ? (
-                                userSolve.concat(Array(fill.length - userSolve.length).fill('_')).join(' ')
-                            ) : (
-                                userSolve.join(' ')
-                            )}
-                        </div>
-                        <br />
-                        <div className="display-4">
-                            {rack.join(' ')}
-                        </div>
+        <div>
+            {isDescribed ? (
+                <div className="container mt-5 font-monospace">
+                    <div className="row align-items-center font-monospace">
+                        <h1 className="fs-1">Scrabble</h1>
+                        <p className="pt-5">
+                            In Scrabble, a "bingo" refers to when a player uses all seven of their tiles in a single turn,
+                            earning 50 bonus points. Finding bingos are an essential part of Scrabble gameplay and can be
+                            the difference between winning or losing a close match. This tool will help you recognize 6-,
+                            7-, or 8-letter anagrams of valid Scrabble words. All words in the database are based on the
+                            North American Scrabble dictionary.
+                        </p>
+                        <p>Controls:</p>
+                        <ul>
+                            <li className="list-unstyled">{'- [Space] ---> reveal answer/definition'}</li>
+                            <li className="list-unstyled">{'- [Delete] --> undo letter'}</li>
+                            <li className="list-unstyled">{'- [Tab] -----> bring this page back'}</li>
+                        </ul>
+                        <p>
+                            Press any key to begin.
+                        </p>
                     </div>
-                ) : (
-                    <div className="display-2">
-                        Loading...
-                    </div>
-                )}
-            </div>
-            {isRevealed && isLoaded ? (
-                <div className="text-left">
-                    <p>{dataList[indexNum].word}</p>
-                    <p className="text-muted">{dataList[indexNum].definition}</p>
                 </div>
             ) : (
-                <div className="w-100"></div>
+                <div className="container mt-5">
+                    <div className="text-center my-5 py-5 font-monospace">
+                        {isLoaded ? (
+                            <div>
+                                <div className="display-2">
+                                    {(userSolve.length < fill.length) ? (
+                                        userSolve.concat(Array(fill.length - userSolve.length).fill('_')).join(' ')
+                                    ) : (
+                                        userSolve.join(' ')
+                                    )}
+                                </div>
+                                <br />
+                                <div className="display-2">
+                                    {rack.join(' ')}
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="display-2">
+                                Loading...
+                            </div>
+                        )}
+                    </div>
+                    {isRevealed && isLoaded ? (
+                        <div>
+                            <p>{dataList[indexNum].word}</p>
+                            <p className="text-muted">{dataList[indexNum].definition}</p>
+                        </div>
+                    ) : (
+                        <div className="w-100"></div>
+                    )}
+                </div>
             )}
         </div>
     );
