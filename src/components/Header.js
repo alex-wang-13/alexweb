b42ea5a5de6de08ee4f0cbc12810a6182f567259
Alex Wang <iamalex11303@gmail.com>
2024-11-05 16:18:14 -0600

    modified header to only display home dropdown tab on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,13 +40,13 @@ const Header = () => {
     return (
         <nav className="navbar navbar-expand-sm navbar-light bg-light p-3">
             <div className="container-fluid">
-                <a className="navbar-brand">AlextheDev</a>
+                <a className="navbar-brand" href="/">AlextheDev</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
-                        <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
+                        {isMobile && <li className="nav-item"><a className="nav-link" href="/">Home</a></li>}
                         <li className="nav-item"><a className="nav-link" href="/posts">Posts</a></li>
                         {!isMobile &&
                             <li className="nav-item dropdown">
