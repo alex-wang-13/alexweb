0ae8717c536a9a3e63bca522ffcc5c2de5d8ac31
Alex Wang <iamalex11303@gmail.com>
2024-11-15 13:37:11 -0600

    added conditional rendering of board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import PostList from './components/PostList';
@@ -11,24 +11,33 @@ import Form from './components/Form';
 import Profile from './components/Profile';
 import Board from './components/Board';
 
+const Layout = () => {
+  const location = useLocation();
+
+  return (
+    <div className="container-fluid">
+      {/* Conditionally render Header */}
+      {location.pathname !== '/board' && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<PostList />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/anagram" element={<Anagram />} />
+        <Route path="/crossword" element={<Crossword />} />
+        <Route path="/compose" element={<Compose />} />
+        <Route path="/register" element={<Form action="Register" />} />
+        <Route path="/login" element={<Form action="Login" />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <div className="container-fluid">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<PostList />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/anagram" element={<Anagram />} />
-          <Route path="/crossword" element={<Crossword />} />
-          <Route path="/compose" element={<Compose />} />
-          <Route path="/register" element={<Form action="Register" />} />
-          <Route path="/login" element={<Form action="Login" />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/board" element={<Board />} />
-        </Routes>
-      </div>
+      <Layout />
     </Router>
   );
 }
